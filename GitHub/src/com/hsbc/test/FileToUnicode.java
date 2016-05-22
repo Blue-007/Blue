bcbfcf02651662fdcb9541f795a377fba9b834b0
Blue <1760703571@qq.com>
2016-05-22 10:48:17 +0800

    测试

---
+++ GitHub/src/com/hsbc/test/FileToUnicode.java
@@ -0,0 +1,13 @@
+package com.hsbc.test;
+import java.io.*;
+
+public class FileToUnicode {
+	public static void main(String[] args)throws IOException{
+		String s;
+		InputStreamReader in  = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(in);
+		while((s=reader.readLine())!=null){
+			System.out.println("echo:"+s);
+		}
+	}
+}
